c0653d9493cd6d91b13149bd95a6c79c2c67f58e
derovi <derovi@users-MacBook-Pro.local>
2022-10-14 12:51:17 +0300

    OKX optimized

--- src/main/kotlin/by/derovi/botp2p/exchange/exchanges/OKX.kt
+++ src/main/kotlin/by/derovi/botp2p/exchange/exchanges/OKX.kt
@@ -5,51 +5,62 @@ import by.derovi.botp2p.exchange.NetworkUtils
 import com.fasterxml.jackson.databind.ObjectMapper
 
 object OKX : Exchange {
-
     override fun fetch(
         orderType: OrderType,
         token: Token,
         currency: Currency,
         paymentMethod: PaymentMethod
-    ): List<Offer> {
-        val result = mutableListOf<Offer>()
-        val url = requestUrl(token, currency, orderType, paymentMethod, 1)
-        val data = NetworkUtils.getRequest(url)
-        result.addAll(parseResponse(token, currency, url, orderType, data))
-        result.forEach {
-            it.paymentMethod = paymentMethod
-            it.exchange = this
+    ): List<Offer> = listOf()
+
+    override fun getFetchTasks(): List<() -> Map<Setup, List<Offer>>> {
+        val fetchTasks = mutableListOf<() -> Map<Setup, List<Offer>>>()
+        for (token in supportedTokens()) {
+            for (currency in supportedCurrencies()) {
+                for (orderType in OrderType.values()) {
+                    fetchTasks.add {
+                        val url = requestUrl(token, currency, orderType)
+                        val data = NetworkUtils.getRequest(url)
+                        return@add parseResponse(token, currency, orderType, data).groupBy {
+                            Setup(token, currency, this, it.paymentMethod!!, orderType)
+                        }
+                    }
+                }
+            }
         }
-        return result
+        return fetchTasks
     }
 
-    fun parseResponse(token: Token, currency: Currency, url: String, orderType: OrderType, data: String): List<Offer> {
-        return ObjectMapper().readTree(data)["data"][if (orderType == OrderType.BUY) "sell" else "buy"].map {
-            Offer(
-                it["price"].asDouble(),
-                token,
-                orderType,
-                it["availableAmount"].asDouble(),
-                it["quoteMinAmountPerOrder"].asDouble(),
-                it["quoteMaxAmountPerOrder"].asDouble(),
-                it["nickName"].asText(),
-                (it["completedRate"].asDouble() * 100).toInt(),
-                it["completedOrderQuantity"].asInt(),
-                true,
-                "https://www.okx.com/ru/p2p-markets/" +
-                        "${currencyToCode[currency]}/" +
-                        "${if (orderType == OrderType.BUY) "buy" else "sell"}-${tokenToCode[token]}"
-            )
-        }
+    fun parseResponse(token: Token, currency: Currency, orderType: OrderType, data: String): List<Offer> {
+        return ObjectMapper().readTree(data)["data"][if (orderType == OrderType.BUY) "sell" else "buy"].map { entry ->
+            entry["paymentMethods"].mapNotNull { codeToPaymentMethod[it.asText()] }.map { paymentMethod ->
+                Offer(
+                    entry["price"].asDouble(),
+                    token,
+                    orderType,
+                    entry["availableAmount"].asDouble(),
+                    entry["quoteMinAmountPerOrder"].asDouble(),
+                    entry["quoteMaxAmountPerOrder"].asDouble(),
+                    entry["nickName"].asText(),
+                    (entry["completedRate"].asDouble() * 100).toInt(),
+                    entry["completedOrderQuantity"].asInt(),
+                    true,
+                    "https://www.okx.com/ru/p2p-markets/" +
+                            "${currencyToCode[currency]}/" +
+                            "${if (orderType == OrderType.BUY) "buy" else "sell"}-${tokenToCode[token]}",
+                    paymentMethod,
+                    this
+                )
+            }
+        }.flatten()
     }
 
-    fun requestUrl(token: Token, currency: Currency, orderType: OrderType, paymentMethod: PaymentMethod, page: Int) =
+    fun requestUrl(token: Token, currency: Currency, orderType: OrderType) =
         "https://www.okx.com/v3/c2c/tradingOrders/books" +
                 "?t=${System.currentTimeMillis()}" +
                 "&quoteCurrency=${currencyToCode[currency]}" +
                 "&baseCurrency=${tokenToCode[token]}" +
                 "&side=${if (orderType == OrderType.BUY) "sell" else "buy"}" +
-                "&paymentMethod=${paymentMethodToCode[paymentMethod]}" +
+                "&paymentMethod=all" +
                 "&userType=all" +
                 "&showTrade=false" +
                 "&showFollow=false" +
@@ -67,29 +78,29 @@ object OKX : Exchange {
         Currency.RUB to "rub",
     )
 
-    private val paymentMethodToCode = mapOf(
-        PaymentMethod.AK_BARS_BANK to "Ak+Bars+Bank",
-        PaymentMethod.ALFA_BANK to "Alfa+Bank",
-        PaymentMethod.BANK_TRANSFER to "bank",
-        PaymentMethod.GAZPROMBANK to "Gazprombank",
-        PaymentMethod.MKB to "MKB",
-        PaymentMethod.OTKRITIE to "Otkritie",
-        PaymentMethod.PAYEER to "Payeer",
-        PaymentMethod.POSTBANK to "Pochta+Bank",
-        PaymentMethod.PSB to "PSB",
-        PaymentMethod.QIWI to "QiWi",
-        PaymentMethod.RAIFAIZEN to "Raiffaizen",
-        PaymentMethod.ROSBANK to "Rosbank",
-        PaymentMethod.ROSSELHOZBANK to "Rosselhozbank",
-        PaymentMethod.RUSSIANSTANDARD to "Russian+Standard+Bank",
-        PaymentMethod.SBERBANK to "Sberbank",
-        PaymentMethod.SBP_TRANSFER to "SBP+Fast+Bank+Transfer",
-        PaymentMethod.SOVCOMBANK to "Sovcombank",
-        PaymentMethod.TINKOFF to "Tinkoff",
-        PaymentMethod.UNICREDIT to "UniCredit+Bank",
-        PaymentMethod.URALSIB to "Uralsib+Bank",
-        PaymentMethod.VTB to "VTB",
-        PaymentMethod.YANDEXMONEY to "Yandex.Money"
+    private val codeToPaymentMethod = mapOf(
+        "Ak+Bars+Bank" to PaymentMethod.AK_BARS_BANK,
+        "Alfa+Bank" to PaymentMethod.ALFA_BANK,
+        "bank" to PaymentMethod.BANK_TRANSFER,
+        "Gazprombank" to PaymentMethod.GAZPROMBANK,
+        "MKB" to PaymentMethod.MKB,
+        "Otkritie" to PaymentMethod.OTKRITIE,
+        "Payeer" to PaymentMethod.PAYEER,
+        "Pochta+Bank" to PaymentMethod.POSTBANK,
+        "PSB" to PaymentMethod.PSB,
+        "QiWi" to PaymentMethod.QIWI,
+        "Raiffaizen" to PaymentMethod.RAIFAIZEN,
+        "Rosbank" to PaymentMethod.ROSBANK,
+        "Rosselhozbank" to PaymentMethod.ROSSELHOZBANK,
+        "Russian+Standard+Bank" to PaymentMethod.RUSSIANSTANDARD,
+        "Sberbank" to PaymentMethod.SBERBANK,
+        "SBP+Fast+Bank+Transfer" to PaymentMethod.SBP_TRANSFER,
+        "Sovcombank" to PaymentMethod.SOVCOMBANK,
+        "Tinkoff" to PaymentMethod.TINKOFF,
+        "UniCredit+Bank" to PaymentMethod.UNICREDIT,
+        "Uralsib+Bank" to PaymentMethod.URALSIB,
+        "VTB" to PaymentMethod.VTB,
+        "Yandex.Money" to PaymentMethod.YANDEXMONEY
     )
 
     override fun supportedTokens(): Array<Token> {
@@ -97,7 +108,7 @@ object OKX : Exchange {
     }
 
     override fun supportedPaymentMethods(): Array<PaymentMethod> {
-        return paymentMethodToCode.keys.toTypedArray()
+        return codeToPaymentMethod.values.toTypedArray()
     }
 
     override fun supportedCurrencies(): Array<Currency> {
